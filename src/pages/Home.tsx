4199727ccf844c31022c847f1907620855df07cc
louis vastel <l.vastel@myskolae.fr>
2025-10-04 13:05:13 +0200

    rien

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,11 +4,13 @@ function Home() {
     return (
         <body>
             <header>
-                <div>
-                    <img src="src\assets\NasaSA_logo.png" alt="logo"/>
-                    <h1>Oxc4t Presents</h1>
-                </div>
+                <img src="src\assets\NasaSA_logo.png" alt="logo"/>
+                <h1>Oxc4t Presents</h1>
             </header>
+            <main>
+                <h1>Will it rain on my parade</h1>
+                <p>An app that absolutely will not work lolololol</p>
+            </main>
         </body>
     )
 }
