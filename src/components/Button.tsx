f940c1f058fe0b10ed6d81d5a594414f22ded65d
OzymandiasKnight <ozymandias.knight@gmail.com>
2025-10-04 11:55:16 +0200

    Adding Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,13 @@
-import '../App.css';
 
-function Button() {
+type propsButton = {
+    name: string,
+};
 
+function Button(props: propsButton) {
 
     return (
         <button>
-            <div>
-
-            </div>
+            <h1>{props.name}</h1>
         </button>
     );
 }
