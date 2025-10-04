721f34fbc9c0e31126f8eec3382140bf9d70981c
louis vastel <l.vastel@myskolae.fr>
2025-10-04 13:59:09 +0200

    header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,21 @@
 import "../styles/home.css"
+import Surface from "../components/Surface";
 
 function Home() {
     return (
         <body>
+            <Surface>
             <header>
                 <img src="src\assets\NasaSA_logo.png" alt="logo"/>
                 <h1>Oxc4t Presents</h1>
             </header>
+            </Surface>
+            <Surface>
             <main>
                 <h1>Will it rain on my parade</h1>
                 <p>An app that absolutely will not work lolololol</p>
             </main>
+            </Surface>
         </body>
     )
 }
