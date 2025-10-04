a7563dcb612a936dda131acf479810c9a6f3fe06
Kagamiie <cakic.lukasrb@gmail.com>
2025-10-04 13:51:42 +0200

    .

---
+++ src/components/Button.tsx
@@ -0,0 +1,14 @@
+type propsButton = {
+  name: string;
+  click: () => void;
+};
+
+function Button(props: propsButton) {
+  return (
+    <button onClick={props.click}>
+      <h1>{props.name}</h1>
+    </button>
+  );
+}
+
+export default Button;
