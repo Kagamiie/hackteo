7213f34b30139cfbec947711c7f041faf906e1e5
louis vastel <l.vastel@myskolae.fr>
2025-10-04 12:28:28 +0200

    small changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,14 @@
+import "../styles/home.css"
+
 function Home() {
     return (
         <body>
-            <h1>Notre superbe site</h1>
+            <header>
+                <div>
+                    <img src="src\assets\NasaSA_logo.png" alt="logo"/>
+                    <h1>Oxc4t Presents</h1>
+                </div>
+            </header>
         </body>
     )
 }
